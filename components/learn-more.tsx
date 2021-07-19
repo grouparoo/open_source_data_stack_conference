20f4c6b61d7d7bcf62b41f9268dc40c59d0ad1af
Brian Leonard <brian@bleonard.com>
2021-07-19 09:49:25 -0500

    No learn more.

--- components/learn-more.tsx
+++ components/learn-more.tsx
@@ -19,9 +19,6 @@ import styleUtils from './utils.module.css';
 import styles from './contact.module.css';
 
 export default function LearnMore() {
-  return (
-    <div className={cn(styleUtils.appear, styleUtils['appear-fifth'], styles.contact)}>
-      TODO copy here to learn more or put below the hero if necessary.
-    </div>
-  );
+  // copy here to learn more or put below the hero if necessary.
+  return <div className={cn(styleUtils.appear, styleUtils['appear-fifth'], styles.contact)}></div>;
 }
